d3cde85c9c9b506c4b6bb65398dd56ff7e6ea54d
s poorna chandra <poornanaidu1@gmail.com>
2023-12-06 16:59:58 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  public_subnet_ids = [for k,v in lookup(lookup(module.subnets,"public",null),"subnet_ids",null): v.id]
+  app_subnet_ids = [for k,v in lookup(lookup(module.subnets,"app",null),"subnet_ids",null): v.id]
+  db_subnet_ids = [for k,v in lookup(lookup(module.subnets,"db",null),"subnet_ids",null): v.id]
+  private_subnets_ids = local.app_subnet_ids
+}
