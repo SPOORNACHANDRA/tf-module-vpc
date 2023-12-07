5b156845f85b6aeed2922a5283bd752895922994
s poorna chandra <poornanaidu1@gmail.com>
2023-12-07 12:18:27 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,3 +1,5 @@
 variable "cidr" {}  #this is a varabile recieving from the main.tf roboshop-terraform-v1
 variable "subnets" {} #this is a varabile recieving from the main.tf roboshop-terraform-v1
 variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_route_table_id" {}
